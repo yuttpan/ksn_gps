255f3f0984e6bca3b6a07cd687c6ebafed77abb6
yuttp <yuttp@DESKTOP-8577QG8>
2016-06-26 20:37:13 +0700

    edit login.js

--- www/controllers/Login.js
+++ www/controllers/Login.js
@@ -1,13 +1,23 @@
 angular.module('starter')
 
-  .controller('Login',function ($scope,$stateParams,$http,$state) {
+  .controller('Login',function ($scope,$http,$state) {
+      $scope.username = "" ;
     $scope.btnLogin= function (data) {
-      $http.get('http://118.175.76.244:81/HouseGps/check_login.php?username=admin&password=admin').success(function(data){
-var status = data[0].status ;
-if(status = "OK"){
-  $state.go('home');
+              var uname = $scope.username ;
+              console.log(uname);
+
+      $http.get('http://118.175.76.244:81/HouseGps/check_login.php?username=adin&password=admin').success(function(response){
+var d = response[0].status;
+console.log(d);
+if (d=="OK"){
+    console.log("OO");
+    $state.go('home');
+}else{
+        console.log("dd");
+ $state.go('login');
 }
-        console.log(status);
+
+        
 
       }).error(function(error) {
         console.error('error');
