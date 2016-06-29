cc3a51ca093f2946c6ca4b2444bcb64e2a77faa9
yuttp <yuttp@DESKTOP-8577QG8>
2016-06-29 07:40:08 +0700

    edit login.js

--- www/controllers/map.js
+++ www/controllers/map.js
@@ -1,4 +1,40 @@
 angular.module('starter')
-.controller('MyCtrl', function($scope) {
-console.log("Map");
-})
+.controller('MapController', function($scope, $cordovaGeolocation, $ionicLoading, $ionicPlatform) {
+
+    $ionicPlatform.ready(function() {
+
+        $ionicLoading.show({
+            template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+        });
+
+        var posOptions = {
+            enableHighAccuracy: true,
+            timeout: 20000,
+            maximumAge: 0
+        };
+        $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+            var lat  = position.coords.latitude;
+            var long = position.coords.longitude;
+
+            var myLatlng = new google.maps.LatLng(lat, long);
+             $scope.lat = lat ;
+             $scope.long = long ;
+            var mapOptions = {
+                center: myLatlng,
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+            $scope.map = map;
+            $ionicLoading.hide();
+
+        }, function(err) {
+            $ionicLoading.hide();
+            console.log(err);
+        });
+    });
+});
+
+
